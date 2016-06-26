63c0b0f502941206dccc3629e121016ae5e0d897
ainul <ainul.top@gmail.com>
2016-06-26 23:06:29 +0700

    mantab

--- www/js/controllers/ComponentsCtrl.js
+++ www/js/controllers/ComponentsCtrl.js
@@ -1,4 +1,109 @@
-﻿app.controller('ComponentsCtrl', function ($scope, $stateParams, ionicMaterialInk, $http, $ionicPopup,$cordovaGeolocation) {
+﻿app.controller('PulsaCtrl', function ($scope, $stateParams, ionicMaterialInk, $http, $ionicPopup,$cordovaGeolocation) {
+    var posOptions = {timeout: 10000, enableHighAccuracy: true};
+    var tsel = false;
+    $scope.prepaid = {
+        telco : 0,
+        nominal : 0
+    };
+    $scope.valid = true;
+    $cordovaGeolocation
+        .getCurrentPosition(posOptions)
+        .then(function (position) {
+            var lat  = position.coords.latitude
+            var long = position.coords.longitude
+            // console.log();
+            console.log(geolib.isPointInside({latitude: lat, longitude: long},geom));
+            if(geolib.isPointInside({latitude: lat, longitude: long},geom)==true){
+                tsel = true;
+            }else{
+                tsel = false;
+            }
+            $scope.valid = tsel;
+        }, function(err) {
+            // error
+        });
+
+    $scope.checkOperator = function(){
+        console.log($scope.prepaid.telco);
+        console.log($scope.prepaid.nominal);
+        if($scope.prepaid.telco != 'tsel'){
+            $scope.valid = true;
+        }else{
+            $scope.valid = tsel;
+        }
+        console.log($scope.valid);
+    };
+
+    $scope.beli = function () {
+        var response = JSON.parse(window.localStorage.getItem("response"));
+        key = CryptoJS.enc.Utf8.parse(key);
+        var iv = CryptoJS.enc.Base64.parse(response.iv); //nilai iv ada di response
+        var plaintext = CryptoJS.AES.decrypt(response.password_trx, key, {iv: iv, padding: CryptoJS.pad.ZeroPadding}); //kata merupakan password yg terenkripsi
+        // console.log(plaintext.toString());
+        //     var text = CryptoJS.enc.Utf8.stringify(plaintext);
+        var text = plaintext.toString(CryptoJS.enc.Utf8);
+
+        var res = response.username_trx;
+        var password = text;
+        console.log(password);
+        // var shaObj = new jsSHA("SHA-1", "TEXT");
+        // shaObj.update(''+response.username_trx+text+dataWaktu);
+        // var hash = shaObj.getHash("HEX");
+
+        var timestamp = date_php("YmdHis");
+        var pass = CryptoJS.SHA1(('' + res + password + timestamp)).toString();
+
+        /*
+         <option value="0">Tri</option>
+         <option selected="" value="1">Indosat</option>
+         <option value="2">Smartfren</option>
+         <option value="3">Telkomsel</option>
+         <option value="4">XL</option>
+         <option value="5">Esia</option>
+         */
+
+
+        var data = {
+            "userid": response.username_trx,
+            "reffid": "Mobile-" + timestamp + "-" + Math.floor(Math.random() * 700),
+            "target": $scope.data.notelp,
+            "amount": 0,
+            "terminal": "terminal-mobapps-1",
+            "timestamp": timestamp,
+            "sign": pass,
+            "prodName": $scope.prepaid.telco+$scope.prepaid.nominal
+        };
+        // console.log(data);
+        var link_beli = "http://103.16.78.45/admin/index.php/api/routers/router";
+
+        $http({
+            method: 'POST',
+            url: link_beli,
+            data: data,
+            headers: {'Content-Type': 'application/json'}
+        }).then(function (res) {
+
+            if (res.status == 200) {
+                if (res.data.status != "FAILED") {
+                    var alertPopup = $ionicPopup.alert({
+                        title: 'Beli Pulsa Berhasil',
+                        template: 'Selamat anda telah membeli pulsa'
+                    });
+                } else {
+                    var alertPopup = $ionicPopup.alert({
+                        title: 'Beli Pulsa Gagal',
+                        template: res.data.message
+                    });
+                }
+            } else {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Beli Pulsa Gagal',
+                    template: res.data.status
+                });
+            }
+        });
+    }
+}).controller('ComponentsCtrl', function ($scope, $stateParams, ionicMaterialInk, $http, $ionicPopup,$cordovaGeolocation) {
     var posOptions = {timeout: 10000, enableHighAccuracy: true};
     $cordovaGeolocation
         .getCurrentPosition(posOptions)
@@ -15,12 +120,6 @@
         });
 
     $scope.beli = function () {
-
-
-
-
-
-
 
         var response = JSON.parse(window.localStorage.getItem("response"));
 
@@ -89,4 +188,5 @@
             }
         });
     }
-});
+})
+;
